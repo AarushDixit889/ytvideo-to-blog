43bfa5e23943d530dbdf897e73c35286b5857de2
Aarush Dixit <133681987+AarushDixit889@users.noreply.github.com>
2024-03-23 20:48:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,42 @@
 import streamlit as st
+from youtube_transcript_api import YouTubeTranscriptApi
 from groq import Groq
 
-st.set_page_config(page_title="Story Book Generator")
-st.title("Story Book Generator")
-user_message=st.text_input("Title of the book")
+st.set_page_config("Youtube Video Summarization")
 groq=Groq(api_key="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")
+def get_transcript(video_id):
+    transcript_list=YouTubeTranscriptApi.get_transcript(video_id=video_id)
+    return " ".join([entry['text'] for entry in transcript_list])
 
-if st.button('Generate Story'):
-	with st.spinner("Generating Story"):
-		prompt = f"""Generate a 10-20 pages kids story.Here are some of the info that you have to follow-
-        - There will be no abuse word in kids story because kids read that
-        - Use nothing else in the topic that is given
-        - In story book there should least 5 pages of A4 size
-        - Ensure to complete full story
-        - Try to generate a very interesting story
-        - Try to use heading markdown
-		- Ensure that kids learn something from it
-        - Try to use character introduction at the start
-        Now the topic to generate story is "{user_message}",kids story book
-        """
-		content=groq.chat.completions.create(
-			    messages=[
-                        {
-                            "role": "user",
-                            "content": prompt,
-                        }
-                    ],
-                    model="mixtral-8x7b-32768",
-        ).choices[0].message.content
-		st.write(content)
-	
+st.title("Youtube Video Summarizer")
+st.warning("After generating blog read the blog end-to-end.")
+video_url=st.text_input("Enter the url of video")
+
+if st.button("Generate Blog"):
+    with st.spinner("Generating blog"):
+        if video_url=="":
+            st.error("Please provide input url.")
+        if not video_url=="":
+            transcript=get_transcript(video_url.split("?v=")[1])
+
+            content=groq.chat.completions.create(
+                messages=[
+                    {
+                        "role":"user",
+                        "content":f""""
+                        Generate a blog on this youtube video.Transcript is provided here'{transcript}'.Use markdowns if required.Use code markdown if needed.
+                        Use this format in html convert it in markdown-
+                        <h1>Title</h1><p>title</p>
+                        <h1>Table of content</h1><table>Table of content</table>
+                        <h1>Content</h1><p>Content</p>
+                        <h1>FAQs</h1><li>FAQS</li>
+                        <h1>Conclusion</h1><p>conclusion</p>
+                        <h1>Facts</h1><p>FACTS</p>
+                        <h1>Timestamps</h1><table>*(timing clickable and goes to video at that particular time) *(Topic name)</table>
+                        Timestamps must be clickable and should be in table.Add code markdown if required.Only use available images.Automatically write the name of language in which code is coded in.Generate full blog end to end that is given in transcript.My persona is a blogger
+                        """
+                    }
+                ],
+                model="mixtral-8x7b-32768"
+            ).choices[0].message.content
+            st.markdown(content,unsafe_allow_html=True)
